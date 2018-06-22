c52c110e80306ad89baf49c5afd3edaa440c8112
Hassan Chabab <chababhassan@gmail.com>
2018-06-22 22:33:23 +0100

    feat(usecase): Define Usecase methods

---
+++ api/urlshorten/usecase/urlshorten_usecase.go
@@ -0,0 +1,34 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/hoflish/url-shortener/api/models"
+
+	"github.com/hoflish/url-shortener/api/urlshorten"
+)
+
+type urlshortenUsecase struct {
+	urlshortenRepos urlshorten.URLShortenRepository
+	contextTimeout  time.Duration
+}
+
+func NewURLShortenUsecase(u urlshorten.URLShortenRepository, timeout time.Duration) urlshorten.URLShortenRepository {
+	return &urlshortenUsecase{
+		urlshortenRepos: u,
+		contextTimeout:  timeout,
+	}
+}
+
+func (u *urlshortenUsecase) Fetch(ctx context.Context, urlCode string) (*models.URLShorten, error) {
+	panic("Not Implemented")
+}
+
+func (u *urlshortenUsecase) Store(ctx context.Context, urlShorten *models.URLShorten) (string, error) {
+	panic("Not Implemented")
+}
+
+func (u *urlshortenUsecase) Close() {
+	panic("Not implemented")
+}
