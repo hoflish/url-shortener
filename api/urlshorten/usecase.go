ef07bf9cf8983addaf7f0ad762d8c19767e84b2f
Hassan Chabab <chababhassan@gmail.com>
2018-06-22 23:10:30 +0100

    feat(usecase): Create URLShortenUsecase interface

---
+++ api/urlshorten/usecase.go
@@ -0,0 +1,13 @@
+package urlshorten
+
+import (
+	"context"
+
+	"github.com/hoflish/url-shortener/api/models"
+)
+
+// URLShortenUsecase defines methods which must be implemented by usecase handler
+type URLShortenUsecase interface {
+	Fetch(ctx context.Context, urlCode string) (*models.URLShorten, error)
+	Store(ctx context.Context, urlShorten *models.URLShorten) (string, error)
+}
