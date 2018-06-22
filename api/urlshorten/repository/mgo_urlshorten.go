01a539d7268a23fb06425fd51a4fe5615c45bff6
Hassan Chabab <chababhassan@gmail.com>
2018-06-22 20:40:40 +0100

    feat(repository): Implement minimal Fetch method

--- api/urlshorten/repository/mgo_urlshorten.go
+++ api/urlshorten/repository/mgo_urlshorten.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hoflish/url-shortener/api/models"
 	"github.com/hoflish/url-shortener/api/urlshorten"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
@@ -24,7 +25,15 @@ func NewMgoURLShortenRepository(session *mgo.Session) urlshorten.URLShortenRepos
 func (mg *mgoURLShortenRepository) Fetch(ctx context.Context, urlCode string) (*models.URLShorten, error) {
 	repo := NewMgoURLShortenRepository(mg.session)
 	defer repo.Close()
-	panic("Not implemented")
+
+	res := models.URLShorten{}
+	// TODO: move direct operations to database to internal functions
+	if err := mg.collection().Find(bson.M{"url_code": urlCode}).One(&res); err != nil {
+		// TODO: logging conn errors (See: https://github.com/sirupsen/logrus)
+		return nil, err
+	}
+	// TODO: retun error "NOT_FOUND_ERROR" when no data returned
+	return &res, nil
 }
 
 func (mg *mgoURLShortenRepository) Close() {
