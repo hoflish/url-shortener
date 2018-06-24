99b3b4994e18c2b83898f4f2fce6e4ced6f43bb9
Hassan Chabab <chababhassan@gmail.com>
2018-06-24 22:17:27 +0100

    refactor(models) Change NOT_FOUND_ERROR message

--- api/models/errors.go
+++ api/models/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	INTERNAL_SERVER_ERROR = errors.New("Internal Server Error")
-	NOT_FOUND_ERROR       = errors.New("ShortURL not found")
+	NOT_FOUND_ERROR       = errors.New("Url not found")
 )
