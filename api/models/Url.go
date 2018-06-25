bfbd6519ac7103bb9c6c4ac8d52c11de0808a5f8
Hassan Chabab <chababhassan@gmail.com>
2018-06-25 13:09:36 +0100

    refactor(models): Refactor Url model
    * Removed UrlCode field
    * Rename ShortUrl field to UrlId
    * Use UrlId field as resource id

--- api/models/Url.go
+++ api/models/Url.go
@@ -8,10 +8,9 @@ import (
 
 // Url Resource
 type Url struct {
-	ID        bson.ObjectId `bson:"_id" json:"id"` // TODO: rename json id to _id
+	ID        bson.ObjectId `bson:"_id" json:"id"`
 	LongUrl   string        `json:"long_url"`
-	ShortUrl  string        `json:"short_url"` // TODO: rename this field to url_id
-	UrlCode   string        `json:"url_code"`  // TODO: remove this field
+	UrlId     string        `json:"url_id"` // Short URL, e.g. "http://bit.ly/Cv7u".
 	CreatedAt time.Time     `json:"created_at"`
 	UpdatedAt time.Time     `json:"updated_at"`
 }
