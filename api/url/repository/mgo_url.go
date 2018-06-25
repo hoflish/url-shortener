56f65e292ca32b7d3ad1080a380020d091d233fc
Hassan Chabab <chababhassan@gmail.com>
2018-06-25 14:55:04 +0100

    refactor(repository): Change urlcode key by urlid one

--- api/url/repository/mgo_url.go
+++ api/url/repository/mgo_url.go
@@ -25,22 +25,22 @@ func NewMgoUrlRepository(session *mgo.Session) url.UrlRepository {
 }
 
 // Fetch method gets a specified Url Resource
-func (mg *mgoUrlRepository) Fetch(ctx context.Context, urlCode string) (*models.Url, error) {
-	res := models.Url{}
+func (mg *mgoUrlRepository) Fetch(ctx context.Context, urlId string) (*models.Url, error) {
+	result := models.Url{}
 	// TODO: move direct operations to database to internal functions
-	if err := mg.collection().Find(bson.M{"urlcode": urlCode}).One(&res); err != nil {
+	if err := mg.collection().Find(bson.M{"urlid": urlId}).One(&result); err != nil {
 		if err == mgo.ErrNotFound {
 			return nil, models.NOT_FOUND_ERROR
 		}
 		logrus.Error(err)
 		return nil, err
 	}
-	return &res, nil
+	return &result, nil
 }
 
 // Store method stores a new Url Resource
 func (mg *mgoUrlRepository) Store(ctx context.Context, url *models.Url) (*models.Url, error) {
-	panic("Not implemented")
+	panic("Not implemented yet!")
 }
 
 // collection - unexported method - returns mongodb collection
