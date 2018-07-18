53f8e4b81508d38a7e9c179197526a362552e201
Hassan Chabab <chababhassan@gmail.com>
2018-07-18 18:45:01 +0100

    style(web): Reconfig eslint with prettier

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => <h1>Hello World</h1>;
+const Hello = () => <h1>Autem illo aut eaque et consequatur illum et. Occaecati repellat qui.</h1>;
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
